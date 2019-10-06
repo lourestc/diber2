e9c3a329d2d4fb7ea1b5310470fe5aae670ff397
lourestc <tulicloure12@yahoo.com.br>
2019-10-06 19:55:12 -0300

    saves csv files with more results, rename graphs.

--- pytulio/discussion/bendito/results.py
+++ pytulio/discussion/bendito/results.py
@@ -27,25 +27,36 @@ class ResultViewer():
 			
 	def _display_clustering( self, evaluators ):
 	
-		for kr,ev in evaluators.items():
+		with self.resultpath.with_name(self.dataname+"cluster_vmeasures.csv").open('w') as f:
+			f.write("Representation,V-measure")
 		
+		for kr,ev in evaluators.items():
+			
 			if kr == "random":
-				print( " V-measure (rand):", np.mean(ev.results["vmeasure"]), "(mean)" )
+				vmeasure = np.mean(ev.results["vmeasure"])
 				plt.hist( ev.results["vmeasure"], histtype='step', label=ev.rmethod )
 			else:
-				print( " V-measure ("+ev.rmethod+"):", ev.results["vmeasure"] )
+				vmeasure = ev.results["vmeasure"]
 				plt.scatter( ev.results["vmeasure"], 1, label=ev.rmethod )
+				
+			print( " V-measure ("+ev.rmethod+"):", vmeasure )
+			with self.resultpath.with_name(self.dataname+"cluster_vmeasures.csv").open('a') as f:
+				f.write("\n"+ev.rmethod+","+str(vmeasure))
 			
+		plt.xlim(0,1)
 		plt.legend()
 		
 		if self.show_plots:
 			plt.show()
 			
-		plt.savefig( self.resultpath.with_name("cluster_vmeasures.png") )
+		plt.savefig( self.resultpath.with_name(self.dataname+"cluster_vmeasures.png") )
 		plt.clf()
 		
 	def _display_order( self, evaluators ):
 	
+		with self.resultpath.with_name(self.dataname+"order_ksstats.csv").open('w') as f:
+			f.write("Representation,KS-stat")
+		
 		for kr,ev in evaluators.items():
 		
 			if kr == "random":
@@ -54,6 +65,8 @@ class ResultViewer():
 				ls='-'
 				ksstat = ss.ks_2samp(ev.results["order_correlations"], evaluators['random'].results["order_correlations"])
 				print( " KS-stat ("+ev.rmethod+"):", ksstat )
+				with self.resultpath.with_name(self.dataname+"order_ksstats.csv").open('a') as f:
+					f.write("\n"+ev.rmethod+","+str(ksstat))
 				
 			ncorrs = len(ev.results["order_correlations"])
 			y = np.array(range(ncorrs)) / float(ncorrs)
@@ -64,10 +77,11 @@ class ResultViewer():
 			plt.plot( x, y, label=ev.rmethod, linestyle=ls )
 		
 		plt.xlim(-1,1)
+		plt.ylim(0,1)
 		plt.legend()
 		
 		if self.show_plots:
 			plt.show()
 			
-		plt.savefig( self.resultpath.with_name("order_ksstats.png") )
+		plt.savefig( self.resultpath.with_name(self.dataname+"order_ksstats.png") )
 		plt.clf()
