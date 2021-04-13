a152f34e330fb6ae1ba3340bf91983169e56be37
lourestc <tulicloure12@yahoo.com.br>
2021-04-13 18:26:44 -0300

    fixed identation issues

--- pytulio/discussion/dataformats/comseq.py
+++ pytulio/discussion/dataformats/comseq.py
@@ -2,58 +2,58 @@ import jsonlines
 
 class Comseq:
 
-    def __init__( self, fname, thread_key='tid', comment_key='cid', subject_key='did', subject_id=None ):
+	def __init__( self, fname, thread_key='tid', comment_key='cid', subject_key='did', subject_id=None ):
 
-        self.Comments = {} #dict of kc:comment
-        self.Threads = {} #list of comments
-        self.Subjects = {} #list of comments
+		self.Comments = {} #dict of kc:comment
+		self.Threads = {} #list of comments
+		self.Subjects = {} #list of comments
 
-        self.subject_key = subject_key
-        self.thread_key = thread_key
-        self.comment_key = comment_key
+		self.subject_key = subject_key
+		self.thread_key = thread_key
+		self.comment_key = comment_key
 
-        self.has_data = {}
+		self.has_data = {}
 
-        self.dictionary = None
-            
-        with jsonlines.open(fname,"r") as f:
-            for c in f:
-                if subject_id==None or c['did']==subject_id:
-                    self.Comments[c[comment_key]] = c
+		self.dictionary = None
+			
+		with jsonlines.open(fname,"r") as f:
+			for c in f:
+				if subject_id==None or c['did']==subject_id:
+					self.Comments[c[comment_key]] = c
 
-        for c in self.Comments.values():
-            self.Threads.setdefault( c[thread_key], [] ).append( c )
+		for c in self.Comments.values():
+			self.Threads.setdefault( c[thread_key], [] ).append( c )
 
-        for t in self.Threads.values():
-            self.Subjects.setdefault( t[0][subject_key], [] ).append( t )
+		for t in self.Threads.values():
+			self.Subjects.setdefault( t[0][subject_key], [] ).append( t )
 
-        if 'ts' in next(iter(self.Comments.values())):
-            for t in self.Threads.values():
-                t.sort( key=lambda c: c['ts'] )
+		if 'ts' in next(iter(self.Comments.values())):
+			for t in self.Threads.values():
+				t.sort( key=lambda c: c['ts'] )
 
-        self._check_data_availability()
+		self._check_data_availability()
 
-    def _check_data_availability( self ):
-        
-        #Comment timestamp
+	def _check_data_availability( self ):
+		
+		#Comment timestamp
 		self.has_data['ctime'] = 'ts' in next(iter(self.Comments.values()))
 		
 		#Thread number / thread sequence
-        for c in self.Comments.values():
-            if 'tnumber' not in c:
-                self.has_data['tnumber'] = False
-                break
-        else: #nobreak
-            self.has_data['tnumber'] = True
+		for c in self.Comments.values():
+			if 'tnumber' not in c:
+				self.has_data['tnumber'] = False
+				break
+		else: #nobreak
+			self.has_data['tnumber'] = True
 
-        #Thread subject
-        nsubj = len(set([ t[0][self.subject_key] for t in self.Threads.values() ]))
-        self.has_data['subjects'] = nsubj>1
+		#Thread subject
+		nsubj = len(set([ t[0][self.subject_key] for t in self.Threads.values() ]))
+		self.has_data['subjects'] = nsubj>1
 
-        #Subject co-recommendation
-        ...
+		#Subject co-recommendation
+		
 
-    def threadtext_list( self ):
+	def threadtext_list( self ):
 
-        return [ ' '.join([c['c'] for c in t]) for t in self.Threads.values() ]
+		return [ ' '.join([c['c'] for c in t]) for t in self.Threads.values() ]
 
